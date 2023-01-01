a0ced0b78a6215668a5c10b5632299f62dbd5f13
Yair Segal <yair.segal@arkadium.com>
2023-01-01 11:08:04 +0200

    csp moved to vercel only config

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,11 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
-const { NODE_ENV } = process.env;
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
 	kit: {
 		adapter: adapter({ precompress: true }),
-		csp: {
-			mode: "auto",
-			directives: {
-				"default-src": ["self"],
-				"style-src": ["self"],
-				"img-src": ["self", "data:", "res.cloudinary.com", "i.ytimg.com"],
-				"connect-src": NODE_ENV === 'development' ? ["self", "http:"] : ["self", "https:"]
-			},
-		},
 		csrf: {
 			checkOrigin: true
 		},
